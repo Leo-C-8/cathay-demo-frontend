be957af66d9a56ee97ab9a0aec8bc392d926d2d3
leo.chang <lyonlink8@outlook.com>
2025-10-21 16:29:51 +0800

    cloudbuild and url

--- src/api/config.js
+++ src/api/config.js
@@ -2,8 +2,8 @@ const isLocalhost = window.location.hostname === 'localhost';
 
 export const API_BASE_URL_ACCOUNT = isLocalhost
     ? 'http://localhost:8080'
-    : 'https://cathay-demo-account-191169836402.asia-east1.run.app';
+    : 'http://account-service';
 
 export const API_BASE_URL_IMAGE = isLocalhost
     ? 'http://localhost:8081'
-    : 'https://cathay-demo-image-191169836402.asia-east1.run.app';
+    : 'http://image-service';
